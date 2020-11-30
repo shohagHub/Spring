33bcba724a33b5513914a01675aa941dbfb6c14e
Saleh Enam Shohag <shohag402@gmail.com>
2020-11-30 21:39:39 +0600

    Initial spring boot data base project
    Signed-off-by: Saleh Enam Shohag <shohag402@gmail.com>

---
+++ SpringBootDB/src/main/java/shohag/spring/db/SpringBootDbApplication.java
@@ -0,0 +1,13 @@
+package shohag.spring.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDbApplication.class, args);
+	}
+
+}
